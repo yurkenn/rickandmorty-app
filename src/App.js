c032381c17a85c0bce37883c3d6a93a4c3a18458
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-05 23:48:49 +0300

    Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Layout from './components/Layout';
+import Locations from './pages/Locations';
+import Episodes from './pages/Episodes';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:character_id" element={<Detail />} />
+          <Route path="/locations" element={<Locations />} />
+          <Route path="/episodes" element={<Episodes />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
