7c461266d6d230acadb2df5ee7874fdf43a2a0dd
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-06 23:51:17 +0300

    Moved router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import * as React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import Layout from './components/Layout';
-import Locations from './pages/Locations';
-import Episodes from './pages/Episodes';
+import Router from './Router';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/character/:character_id" element={<Detail />} />
-          <Route path="/locations" element={<Locations />} />
-          <Route path="/episodes" element={<Episodes />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
