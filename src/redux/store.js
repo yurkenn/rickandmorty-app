3059148446373093b33bda752d2550762b368397
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-06 23:52:17 +0300

    Update redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import charactersSlice from './charactersSlice';
+import episodesSlice from './episodesSlice';
 
 export default configureStore({
   reducer: {
     characters: charactersSlice,
+    episodes: episodesSlice,
   },
 });
