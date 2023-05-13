44de2a4506fe2ba74165b5fca946e50489be089b
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-13 18:24:55 +0300

    Update Router

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
+
 import Home from '../pages/Home';
 import Detail from '../pages/Detail';
-import Locations from '../pages/Locations';
-import Episodes from '../pages/Episodes';
+
 import Error from '../components/Error';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" errorElement={<Error />} element={<Navbar />}>
-          <Route path="/" errorElement={<Error />} element={<Home />} />
-          <Route path="/character/:character_id" errorElement={<Error />} element={<Detail />} />
-          <Route path="/locations" errorElement={<Error />} element={<Locations />} />
-          <Route path="/episodes" errorElement={<Error />} element={<Episodes />} />
-        </Route>
+        <Route path="/" errorElement={<Error />} element={<Home />} />
+        <Route path="/character/:character_id" errorElement={<Error />} element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
