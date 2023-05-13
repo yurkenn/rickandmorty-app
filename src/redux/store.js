f07e6759c6d546924b48af3ab688dde388b62986
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-13 18:24:43 +0300

    Delete unused codes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import charactersSlice from './charactersSlice';
-import episodesSlice from './episodesSlice';
 
 export default configureStore({
   reducer: {
     characters: charactersSlice,
-    episodes: episodesSlice,
   },
 });
